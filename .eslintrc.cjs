be264346c011023c7cdc4a7609bf9016b0456de2
Scott Beale <sbeale@squareup.com>
2022-05-04 13:07:17 -0400

    chore: clean up some unused dependencies
    feat: asyncWritable mappingWriteFunction passed previous value of store
    
    Revert "feat: asyncWritable mappingWriteFunction passed previous value of store"
    
    This reverts commit 2d916d7e82940883c06668af3e5a27b20462fb41.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     'square',
-    'svelte3',
     '@typescript-eslint', // add the TypeScript plugin
   ],
   extends: ['plugin:square/typescript'],
-  overrides: [
-    // this stays the same
-    {
-      files: ['*.svelte'],
-      processor: 'svelte3/svelte3',
-    },
-  ],
   rules: {
     'func-style': ['error', 'expression'],
-    'import/extensions': [
-      'error',
-      'never',
-      {
-        svelte: 'always',
-        yaml: 'always',
-      },
-    ],
+    'import/extensions': ['error', 'never'],
     'no-console': ['error'],
   },
   env: {
